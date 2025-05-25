f0f8c842848ee55033f642a77e659cb01f40f74b
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-05-25 14:57:38 +0530

    speaking feature added along with some other updates

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import os
 import requests
+from win32com.client import Dispatch
+
 
 # Loading environment variables from .env file in the root directory 
 load_dotenv()
@@ -9,6 +11,10 @@ BACKEND_URL = os.getenv("BACKEND_URL")
 
 if not BACKEND_URL:
     raise ValueError("❌ BACKEND_URL not found in .env file!")
+
+def speak(str):
+    speak = Dispatch(('SAPI.SpVoice'))
+    speak.Speak(str)
 
 def SIGNUP():
 
@@ -20,6 +26,7 @@ def SIGNUP():
     password = input("Enter Your Password: ")
 
     print("🔄 Processing your request, Please Wait...")
+    speak("Processing your request, Please Wait...")
 
     try:
         response = requests.post(
@@ -32,13 +39,13 @@ def SIGNUP():
             }
         )
 
-
         response.raise_for_status()  # Raises error if status code is not 2xx
 
         data = response.json()
 
-        print("✅ Signup successful!")
-        print("Response:", data)
+        print(f"✅ {data['message']}")
+        speak(f"{data['message']}")
+        # print("Response:", data)
 
     except:
         print("❌ Something Went Wrong, Please Try Again Later!:" )
@@ -49,20 +56,61 @@ def SIGNUP():
     print()
 
 
+def SHOW_ALL_USERS():
+    print("\n🔎 Searching...")
+    
+    try:
+        response = requests.get(f"{BACKEND_URL}/api/v1/auth/user")
+
+        response.raise_for_status()  # Raises error if status code is not 2xx
+
+        data = response.json()
+
+        print("✅ --- All the Users in the Database --- ✅\n")
+
+        if (len(data['finalUserArray']) == 0):
+            print("⚠️ No Users Found!")
+            speak("No Users Found!")
+            return
+
+        # print("username\t\temail\t\tcontactNumber")
+        # for user in range(len()):
+        #     print(user['username'], "\t\t", user['email'], "\t\t", user['contactNumber'])
+
+        print("Sno\t\tusername\t\temail\t\tcontactNumber")
+        print("-------------------------------------------------------------------------")
+        
+        for index, user in enumerate(data['finalUserArray'], start=1):
+            print(f"{index}\t\t{user['username']}\t\t{user['email']}\t\t{user['contactNumber']}")
+        print()
+    except:
+        print("❌ Something Went Wrong, Please Try Again Later!:" )
+
+
 
 def main():
     while True:
         print("\nOptions:")
-        print("1 -> SignUp")
-        print("2 -> Exit")
+
+        options = ["1-> SignUp" , "2-> Display All Users" , "3-> EXIT\n"]
+
+        for option in options:
+            print(option)
+
         choice = input("Enter Your Choice: ")
 
         if choice == '1':
             SIGNUP()
+            
         elif choice == '2':
-            print("👋 Exiting...")
+            SHOW_ALL_USERS()
+
+        elif choice == '3':
+            print("👋 Byee bye, See You Sooon...")
+            speak("Byee bye, See You Sooon...")
             break
         else:
             print("⚠️ Invalid Choice! Please try again.")
+    print()
 
 main()
